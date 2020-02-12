38b9687d7a41a231e938bbd037c075eaf66263d5
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-02-12 21:33:17 +0200

    Update Ferma.h

--- FermaNew/include/Ferma.h
+++ FermaNew/include/Ferma.h
@@ -16,10 +16,14 @@ class Ferma
         void ListaCateg();
         void SetFan(int fan){cantitateFan += fan;}
         void SetPorumb (int porumb) {cantitatePorumb += porumb;}
+        void SetBuget(int bugetFerma) {sold += bugetFerma;}
         int GetFan (){return cantitateFan;}
         int GetPorumb(){return cantitatePorumb;}
+        int GetBuget(){return sold;}
         void SituatieFerma();
         void VanzareAnimaleFerma();
+        void VizualizeazaBugetFerma();
+        void CumparaAnimale();
 
 
 
@@ -28,9 +32,11 @@ class Ferma
     private:
         std::vector<Animal> Animalute;
         std::vector<int> Pret;
+        std::vector<int>pretCumparare;
         int cantitateFan;
         int cantitatePorumb;
         int buget;
+        int sold;
 };
 
 #endif // FERMA_H
