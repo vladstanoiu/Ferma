5e8a939061a6408b38a6cdd03c758d8252f7d35d
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-02-12 13:31:39 +0200

    Create Animal.h

---
+++ FermaNew/include/Animal.h
@@ -0,0 +1,25 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+#include <string>
+
+enum TipMancare {porumb , fan};
+enum TipAnimal {porc , oaie , bovina , cal , gaina , gasca , rata , curca};
+class Animal
+{
+    public:
+        Animal();
+        Animal(TipAnimal , TipMancare , int);
+        void AfisareOptiuni();
+        virtual ~Animal();
+        TipAnimal GetSpecie() {return specie;}
+        TipMancare GetTipMancare(){return haleala;}
+        int GetCantitateMancare(){return cantitateMancare;}
+    protected:
+
+    private:
+        int cantitateMancare;
+        TipMancare haleala;
+        TipAnimal specie;
+};
+
+#endif // ANIMAL_H
