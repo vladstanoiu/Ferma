94dc9291e451c15cf877a9d223cb85d044dcc7b3
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-02-14 22:05:59 +0200

    Update Pasare.cpp

--- FermaNew/src/Pasare.cpp
+++ FermaNew/src/Pasare.cpp
@@ -7,6 +7,10 @@ Pasare::Pasare()
 Pasare::Pasare(TipAnimal animalut , TipMancare mancare, int cantitate) : Animal::Animal (animalut , mancare ,cantitate)
 {
 }
+Pasare::Pasare(TipAnimal animalut) : Animal::Animal(animalut)
+{
+
+}
 Pasare::~Pasare()
 {
     //dtor
