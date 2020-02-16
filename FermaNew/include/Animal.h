60e75666405b065acebe3498dff824afe7b0ef28
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-02-16 21:07:50 +0200

    Update Animal.h

--- FermaNew/include/Animal.h
+++ FermaNew/include/Animal.h
@@ -4,13 +4,13 @@
 
 enum TipMancare {porumb , fan};
 enum TipAnimal {porc , oaie , bovina , cal , gaina , gasca , rata , curca};
+
 class Animal
 {
     public:
         Animal();
         Animal(TipAnimal , TipMancare , int);
         Animal(TipAnimal);
-        void AfisareOptiuni();
         virtual ~Animal();
         TipAnimal GetSpecie() {return specie;}
         TipMancare GetTipMancare(){return haleala;}
