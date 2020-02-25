ce4bb0b24dc517aea3326784571245d14e21b8aa
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-02-25 18:50:38 +0200

    Update main.cpp

--- FermaNew/main.cpp
+++ FermaNew/main.cpp
@@ -8,11 +8,12 @@ int main()
 {
     Ferma fermaAnimale;
     Meniu meniu(fermaAnimale);
-
+      //cod care citeste din fisier
     while(meniu.GetAlearga())
     {
         meniu.MeniuPrincipal();
     }
+        //cod care scrie in fisier situatia fermei
 
     return 0;
 }
